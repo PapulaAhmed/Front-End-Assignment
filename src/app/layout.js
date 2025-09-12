dc243864f8b68248d8de15c123cd4e1a0aaab9e0
PapulaAhmed <papulafwad@gmail.com>
2025-09-12 22:29:21 +0300

    Initial Commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,13 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Inter, DM_Sans } from "next/font/google";
+import "@/styles/global.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const dmSans = DM_Sans({
+  variable: "--font-dm-sans",
   subsets: ["latin"],
 });
 
@@ -19,9 +19,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${dmSans.variable} ${inter.variable} antialiased`}>
         {children}
       </body>
     </html>
